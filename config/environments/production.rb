26caba4eca59ea9c137a2e94a035fe66fe6befb3
Mallory Bulkley <mallorybulkley@gmail.com>
2017-02-18 22:46:44 -0500

    Update paperclip settings

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -76,4 +76,6 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  Paperclip.options[:content_type_mappings] = { mp3: "application/octet-stream" }
 end
