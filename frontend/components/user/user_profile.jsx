cb94c14fddbbc4ecfa0752b593279dac5fe6cd6e
Mallory Bulkley <mallorybulkley@gmail.com>
2017-02-21 17:21:41 -0500

    Add basic user profile styling

--- frontend/components/user/user_profile.jsx
+++ frontend/components/user/user_profile.jsx
@@ -1,22 +1,46 @@
 import React from 'react';
 
 class UserProfile extends React.Component {
+  constructor (props) {
+    super(props);
+
+    this.state = { view: "tracks" };
+  }
   componentWillMount () {
     this.props.fetchUser();
     this.props.fetchUserPlaylists();
+  }
+
+  userDisplay () {
+    <h1>hello user</h1>
   }
 
   render () {
     const user = this.props.user;
 
     return (
-      <section className="header">
-        <ul>
-          <li>
-            <h3>{ user ? user.username : "" }</h3>
-          </li>
-        </ul>
-        <img src={user ? user.photo_url : "" }/>
+      <section className="user-profile">
+        <section className="header">
+          <ul>
+            <li>
+              <h2>{ user ? user.username : "" }</h2>
+            </li>
+          </ul>
+          <img src={user ? user.photo_url : "" }/>
+        </section>
+
+        <section className="user-nav">
+          <h1 className={ this.state.view === "tracks" ? "active" : "" }
+            onClick={ () => this.setState({ view: "tracks" }) }>
+            Tracks
+          </h1>
+          <h1 className={ this.state.view === "create" ? "active" : "" }
+            onClick={ () => this.setState({ view: "create" }) }>
+            Playlists
+          </h1>
+        </section>
+
+        
       </section>
     )
   }
