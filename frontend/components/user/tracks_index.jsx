73c45e05e6bb2830d0bb6ab0ea423e3b7724abf1
Mallory Bulkley <mallorybulkley@gmail.com>
2017-02-22 17:14:30 -0500

    Add user tracks to queue

--- frontend/components/user/tracks_index.jsx
+++ frontend/components/user/tracks_index.jsx
@@ -8,6 +8,7 @@ class TracksIndex extends React.Component {
     const tracks = this.props.tracks.map((track) => (
       <StreamIndexItem key={ track.id }
         track={ track }
+        queue={ this.props.tracks }
         receiveCurrentTrack={ this.props.receiveCurrentTrack }
         togglePlay={ this.props.togglePlay }
         currentTrack={ this.props.currentTrack.track && this.props.currentTrack.track.id === track.id ? this.props.currentTrack : false } />
