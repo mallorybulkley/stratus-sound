e4c00306c500ae137bbb4d6eb30170071a4d5eb7
Mallory Bulkley <mallorybulkley@gmail.com>
2017-02-23 10:02:15 -0500

    Add pgSearch gem and table

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220163252) do
+ActiveRecord::Schema.define(version: 20170223141242) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -26,6 +26,16 @@ ActiveRecord::Schema.define(version: 20170220163252) do
 
   add_index "comments", ["track_id"], name: "index_comments_on_track_id", using: :btree
   add_index "comments", ["user_id"], name: "index_comments_on_user_id", using: :btree
+
+  create_table "pg_search_documents", force: :cascade do |t|
+    t.text     "content"
+    t.integer  "searchable_id"
+    t.string   "searchable_type"
+    t.datetime "created_at",      null: false
+    t.datetime "updated_at",      null: false
+  end
+
+  add_index "pg_search_documents", ["searchable_type", "searchable_id"], name: "index_pg_search_documents_on_searchable_type_and_searchable_id", using: :btree
 
   create_table "playlist_tracks", force: :cascade do |t|
     t.integer  "playlist_id", null: false
