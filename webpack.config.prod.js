346185541406275858dfe7f04d332f4bc6f6438b
Mallory Bulkley <mallorybulkley@gmail.com>
2017-03-01 23:29:22 -0500

    Fix React minification warnings

---
+++ webpack.config.prod.js
@@ -0,0 +1,38 @@
+var path = require('path');
+var webpack = require("webpack");
+
+module.exports = {
+  entry: './frontend/stratus_sound.jsx',
+  output: {
+    path: 'app/assets/javascripts',
+    filename: 'bundle.js',
+  },
+  plugins:[
+    new webpack.DefinePlugin({
+      'process.env':{
+        'NODE_ENV': JSON.stringify('production')
+      }
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress:{
+        warnings: true
+      }
+    })
+  ],
+  module: {
+    loaders: [
+      {
+        test: [/\.jsx?$/],
+        exclude: /(node_modules)/,
+        loader: 'babel-loader',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
+  },
+  devtool: 'source-map',
+  resolve: {
+    extensions: ['.js', '.jsx', '*']
+  }
+};
