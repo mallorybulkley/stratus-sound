a0acd7066944dc714a7e26005574157cba28656f
Mallory Bulkley <mallorybulkley@gmail.com>
2017-03-15 18:26:21 -0400

    Update tracks correctly when returning to stream

--- frontend/components/stream/stream.jsx
+++ frontend/components/stream/stream.jsx
@@ -11,7 +11,7 @@ class Stream extends React.Component {
   }
 
   componentDidMount () {
-    this.props.fetchScrollTracks(this.state.page);
+    this.props.fetchTracks();
   }
 
   handleInfiniteLoad () {
