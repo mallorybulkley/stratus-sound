b4fb14ddfe576c3eae4f20d2de5d6e1e288231a8
Mallory Bulkley <mallorybulkley@gmail.com>
2017-03-15 11:17:50 -0400

    Count plays from users who aren't logged in

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170224172529) do
+ActiveRecord::Schema.define(version: 20170315151421) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -59,7 +59,7 @@ ActiveRecord::Schema.define(version: 20170224172529) do
 
   create_table "plays", force: :cascade do |t|
     t.integer  "track_id",   null: false
-    t.integer  "user_id",    null: false
+    t.integer  "user_id"
     t.datetime "created_at"
     t.datetime "updated_at"
   end
