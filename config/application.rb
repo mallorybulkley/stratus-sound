5146c8a511c980af23d0b7a65711d2ace75135ca
Mallory Bulkley <mallorybulkley@gmail.com>
2017-03-16 14:13:22 -0400

    Use SSL for AWS files

--- config/application.rb
+++ config/application.rb
@@ -31,6 +31,7 @@ module StratusSound
 
     config.paperclip_defaults = {
       :storage => :s3,
+      :s3_protocol => :https,
       :s3_credentials => {
         :bucket => ENV["s3_bucket"],
         :access_key_id => ENV["s3_access_key_id"],
